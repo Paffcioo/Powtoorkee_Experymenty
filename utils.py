87d3e81fd9c5986ee151a2162d31826cafadbd32
Paweł Przybyła <pawel.przybyla@gmail.com>
2023-08-03 23:46:32 +0200

    trzeci commit - dodanie utils.py

--- utils.py
+++ utils.py
@@ -6,4 +6,11 @@ def character_multiplier(character, multiplier):
 # print(character_multiplier(b, 10))
 
 def mnożenie_przez_pi(liczba):
-    return 3.14*int(liczba)
+    from math import pi
+    return pi*int(liczba)
+
+coś = [2, 3, 4]
+def foo1(*arg):
+    return mnożenie_przez_pi(arg)
+
+print(foo1(coś))
